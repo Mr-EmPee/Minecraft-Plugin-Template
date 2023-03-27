02f1b4bcd31f1e9d0eb84daac014320cfb8c69fd
Manuel Palumbo <manuel.palumbo@email.com>
2023-03-27 17:49:42 +0200

    Removed utility files

--- src/main/java/ml/empee/templateplugin/controllers/PluginController.java
+++ src/main/java/ml/empee/templateplugin/controllers/PluginController.java
@@ -6,31 +6,44 @@ import lombok.RequiredArgsConstructor;
 import ml.empee.ioc.Bean;
 import ml.empee.templateplugin.config.CommandsConfig;
 import ml.empee.templateplugin.utils.Logger;
+import org.bukkit.Bukkit;
+import org.bukkit.block.ShulkerBox;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Shulker;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.Vector;
 import org.jetbrains.annotations.Nullable;
 
 @RequiredArgsConstructor
 public class PluginController implements Bean {
 
   private final CommandsConfig commandsConfig;
+  private final JavaPlugin plugin;
 
   @Override
   public void onStart() {
     commandsConfig.register(this);
   }
 
-  @CommandMethod("echo <text> [target]")
+  @CommandMethod("echo")
   public void sendEcho(
-    CommandSender sender,
-    @Argument String text,
-    @Argument @Nullable Player target
+    Player sender
   ) {
-    if (target != null) {
-      sender = target;
+    Shulker shulker = sender.getLocation().getNearbyEntitiesByType(Shulker.class, 2).stream()
+        .findAny().orElse(null);
+
+    if(shulker == null) {
+      return;
     }
 
-    Logger.log(sender, text);
+    Bukkit.getScheduler().runTaskTimer(plugin,
+      () -> {
+        Vector vector = new Vector(0, 0, 0.1);
+        shulker.teleport(shulker.getLocation().add(vector));
+      }, 0, 1
+    );
   }
 
 }
--- src/main/java/ml/empee/templateplugin/utils/Translator.java
+++ src/main/java/ml/empee/templateplugin/utils/Translator.java
@@ -2,25 +2,56 @@ package ml.empee.templateplugin.utils;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
+import org.apache.commons.io.FileUtils;
+import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Translator {
-  private static final ResourceBundle bundle = ResourceBundle.getBundle("messages", Locale.ROOT);
+  private static final JavaPlugin plugin = JavaPlugin.getProvidingPlugin(Translator.class);
+  private static final ResourceBundle bundle;
+
+  static {
+    bundle = getResourceBundle(Locale.getDefault());
+  }
+
+  @SneakyThrows
+  private static ResourceBundle getResourceBundle(Locale locale) {
+    File messageFile = new File(plugin.getDataFolder(), "messages.properties");
+    if(!messageFile.exists()) {
+      messageFile.getParentFile().mkdirs();
+      FileUtils.copyInputStreamToFile(plugin.getResource("messages.properties"), messageFile);
+    }
+
+    URL url = messageFile.getParentFile().toURI().toURL();
+    return ResourceBundle.getBundle("messages", locale, new URLClassLoader(new URL[]{url}));
+  }
 
   public static String translate(String key) {
     return bundle.getString(key);
   }
+  public static String translate(String key, Locale locale) {
+    return getResourceBundle(locale).getString(key);
+  }
 
-  public static String[] translateBlock(String key) {
-    String translation = Translator.translate(key);
-    if(translation.endsWith("\n")) {
-      translation += " ";
+  private static String[] parseBlock(String input) {
+    if(input.endsWith("\n")) {
+      input += " ";
     }
 
-    return translation.split("\n");
+    return input.split("\n");
+  }
+  public static String[] translateBlock(String key) {
+    return parseBlock(translate(key));
+  }
+  public static String[] translateBlock(String key, Locale locale) {
+    return parseBlock(Translator.translate(key, locale));
   }
 
 }
