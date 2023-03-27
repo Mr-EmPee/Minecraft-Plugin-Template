46072241669f85244db3af1ab681211c0c4aeb75
Manuel Palumbo <manuel.palumbo@email.com>
2023-03-27 17:50:07 +0200

    Fix: Prefix is now translatable

--- src/main/java/ml/empee/templateplugin/TemplatePlugin.java
+++ src/main/java/ml/empee/templateplugin/TemplatePlugin.java
@@ -3,15 +3,16 @@ package ml.empee.templateplugin;
 import lombok.Getter;
 import ml.empee.ioc.SimpleIoC;
 import ml.empee.templateplugin.config.DatabaseConfig;
+import ml.empee.templateplugin.utils.Translator;
 import org.bukkit.plugin.java.JavaPlugin;
 
 /** Boot class of this plugin. **/
 
 public final class TemplatePlugin extends JavaPlugin {
 
-  public static final String PREFIX = "  &5Temp &8»&r ";
-  private static final String SPIGOT_PLUGIN_ID = "";
-  private static final Integer METRICS_PLUGIN_ID = 0;
+  public static final String PREFIX = Translator.translate("prefix");
+  //private static final String SPIGOT_PLUGIN_ID = "";
+  //private static final Integer METRICS_PLUGIN_ID = 0;
 
   private final DatabaseConfig databaseConfig = new DatabaseConfig(this);
 
