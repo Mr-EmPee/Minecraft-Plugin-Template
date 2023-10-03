345541da7cafdf7a8a5e728bd915435c930975c5
Manuel Palumbo <manuel.palumbo@email.com>
2023-10-03 17:30:45 +0200

    Improvement: Abstracted in-memory cache

--- src/main/java/ml/empee/templateplugin/model/entities/Entity.java
+++ src/main/java/ml/empee/templateplugin/model/entities/Entity.java
@@ -6,4 +6,9 @@ package ml.empee.templateplugin.model.entities;
 
 public interface Entity {
   Long getId();
+
+  /**
+   * @return a copy of this entity with the given id
+   */
+  Entity withId(Long id);
 }
