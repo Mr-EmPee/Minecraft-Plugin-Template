f5cd693c2acb6c2d65576cbf7e04809a61d07f43
Manuel Palumbo <manuel.palumbo@email.com>
2023-11-24 14:33:27 +0100

    Fix: Better theme registry

--- src/main/java/ml/empee/templateplugin/controllers/PluginController.java
+++ src/main/java/ml/empee/templateplugin/controllers/PluginController.java
@@ -3,7 +3,6 @@ package ml.empee.templateplugin.controllers;
 import lombok.RequiredArgsConstructor;
 import ml.empee.templateplugin.config.MessageConfig;
 import ml.empee.templateplugin.registries.ItemRegistry;
-import ml.empee.templateplugin.registries.ThemeRegistry;
 import ml.empee.templateplugin.utils.Utils;
 import mr.empee.lightwire.annotations.Singleton;
 import org.bukkit.command.CommandSender;
@@ -13,13 +12,11 @@ import org.bukkit.command.CommandSender;
 public class PluginController {
 
   private final ItemRegistry itemRegistry;
-  private final ThemeRegistry themeRegistry;
   private final MessageConfig messageConfig;
 
   public void reload(CommandSender sender) {
     messageConfig.reload();
     itemRegistry.reload();
-    themeRegistry.reload();
 
     Utils.log(sender, "&7The plugin has been reloaded");
   }
